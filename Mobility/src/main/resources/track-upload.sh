289ef43aa08b441e207afa7005749680c11c8ae6
Janine Son <janine@dfrc.ch>
2024-03-12 18:35:09 +0900

    Update track upload script - unzip first then upload

--- Mobility/src/main/resources/track-upload.sh
+++ Mobility/src/main/resources/track-upload.sh
@@ -3,21 +3,32 @@
 source /opt/utils/config.sh
 set -e
 
-track_file=$(ls -p /data/sites/*/realtime/tracks/ | grep "^site[0-9]*_[0-9]")
+root=/opt/utils
+zip_file=$(ls -p /data/sites/*/tracks/ | grep "^site[0-9]*_[0-9]")
 
-for track in $track_file
+for f in $zip_file
 do
-  echo $track
-  site=${track%%"_"*}
+  site=${f%%"_"*}
   site=$(echo $site | sed 's/site//g')
 
+  parent=/data/sites/site$site/tracks
+  echo $parent/$f
+  unzip $parent/$f
   dataset=$(cat /opt/utils/site-mapping.csv | grep $site | cut -d, -f2)
-  echo $dataset 
+  echo $dataset
 
-  url='https://'$server_name':'$https_port'/utip-dam/mobility/'$dataset
+  url='https://localhost:8081/utip-dam/mobility/upload/1'
 
   echo $url
-  path=/data/sites/site$site/realtime/tracks/$track
 
-  curl -X POST "${url}" --form file=@"${path}"
+  track_file=$(ls $root/data/sites/site$site/tracks/ | xargs -n 1 basename)
+
+  for t in $track_file
+  do
+    track_file_path=$root/data/sites/site$site/tracks/$t
+    curl -X POST "${url}" --form file=@"${track_file_path}"
+    rm $track_file_path
+  done
+
 done
+
