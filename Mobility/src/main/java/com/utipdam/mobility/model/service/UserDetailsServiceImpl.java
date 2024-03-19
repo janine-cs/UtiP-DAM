11376da417af45d253098f3c582f62cd8a809b08
Janine Son <janine@dfrc.ch>
2024-03-19 14:36:11 +0900

    Clean up

--- Mobility/src/main/java/com/utipdam/mobility/model/service/UserDetailsServiceImpl.java
+++ Mobility/src/main/java/com/utipdam/mobility/model/service/UserDetailsServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -15,7 +14,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     UserRepository userRepository;
 
     @Override
-    @Transactional
+    //@Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
