d778285b5220487a65f7c8a9cdc1e28fe49b6b67
Janine Son <janine@dfrc.ch>
2024-04-16 17:27:47 +0900

    Update db diagram

--- Mobility/src/main/java/com/utipdam/mobility/model/entity/User.java
+++ Mobility/src/main/java/com/utipdam/mobility/model/entity/User.java
@@ -37,7 +37,7 @@ public class User {
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(  name = "user_roles",
+    @JoinTable(  name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
