9e0db699ce896b93d3dc7c9d3b6c76156f0f76e3
Janine Son <janine@dfrc.ch>
2024-04-30 14:00:04 +0900

    Update audit api

--- Mobility/src/main/java/com/utipdam/mobility/controller/MobilityController.java
+++ Mobility/src/main/java/com/utipdam/mobility/controller/MobilityController.java
@@ -859,7 +859,7 @@ public class MobilityController {
         }
     }
 
-    @GetMapping("/mobility/audit")
+    @PostMapping("/mobility/audit")
     public ResponseEntity<Map<String, Object>> audit(@RequestPart MultipartFile file,
                                                      @RequestPart String k) {
 
